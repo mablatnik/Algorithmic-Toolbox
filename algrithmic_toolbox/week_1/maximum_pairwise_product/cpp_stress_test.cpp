dd457c3eee89a69ba1158431ccf37177c19b4468
Mark Blatnik <mablatnik@gmail.com>
2016-12-06 10:36:29 +0300

    Commit basic data structures programming assignment

--- algrithmic_toolbox/week_1/maximum_pairwise_product/cpp_stress_test.cpp
+++ algrithmic_toolbox/week_1/maximum_pairwise_product/cpp_stress_test.cpp
@@ -37,7 +37,7 @@ long long MaxPairwiseProductFast(const vector<int>& numbers) {
 
 int main() {
   while (true) {
-    int n = rand() % 998 + 2;
+    int n = rand() % 1000 + 2;
     cout << n << "\n";
     vector<int> a;
     for (int i = 0; i < n; ++i) {
