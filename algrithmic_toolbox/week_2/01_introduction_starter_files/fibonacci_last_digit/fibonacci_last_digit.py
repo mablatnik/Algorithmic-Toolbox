b15dbde2c92544c2789fee0bab86f59c4eb275c3
Mark Blatnik <mablatnik@gmail.com>
2016-12-07 16:27:11 +0300

    Python solution for Last Difit of a Large Fibonacci Number
    Max time used: 0.15/5.00, max memory used: 8712192/536870912

--- algrithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_last_digit/fibonacci_last_digit.py
+++ algrithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_last_digit/fibonacci_last_digit.py
@@ -1,5 +1,5 @@
 # Uses python3
-import sys
+import sys, unittest
 
 def get_fibonacci_last_digit_naive(n):
     if n <= 1:
@@ -13,7 +13,32 @@ def get_fibonacci_last_digit_naive(n):
 
     return current % 10
 
+
+def get_fibonacci_last_digit_fast(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous, current = current % 10, (previous + current) % 10
+
+    return current
+
+class MyTest(unittest.TestCase):
+    def test_naive(self):
+        self.assertEqual( get_fibonacci_last_digit_naive(3), 2)
+        self.assertEqual( get_fibonacci_last_digit_naive(331), 9)
+        self.assertEqual( get_fibonacci_last_digit_naive(327305), 5)
+
+    def test_fast(self):
+        self.assertEqual( get_fibonacci_last_digit_fast(3), 2)
+        self.assertEqual( get_fibonacci_last_digit_fast(331), 9)
+        self.assertEqual( get_fibonacci_last_digit_fast(327305), 5)
+
 if __name__ == '__main__':
+    # unittest.main()
     input = sys.stdin.read()
     n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    print(get_fibonacci_last_digit_fast(n))
