250116cb122ef4b170d7ce686c3a4f1b6dcb3d8b
Mark Blatnik <mablatnik@gmail.com>
2016-12-08 09:24:32 +0300

    C++ solution to GCD
    0.00/1.00, max memory used: 9564160/536870912

--- algrithmic_toolbox/week_2/01_introduction_starter_files/gcd/gcd.cpp
+++ algrithmic_toolbox/week_2/01_introduction_starter_files/gcd/gcd.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using std::cin;
+using std::cout;
 
 int gcd_naive(int a, int b) {
   int current_gcd = 1;
@@ -12,9 +14,35 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int euclid_gcd(int a, int b) {
+  int divisor = a >= b ? a : b;
+  int dividend = a <= b ? a : b;
+  while(divisor != 0) {
+    int remainder = dividend % divisor;
+    dividend = divisor;
+    divisor = remainder;
+  }
+  return dividend;
+}
+
 int main() {
   int a, b;
-  std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
-  return 0;
+  while (true) {
+    a = rand() % 1000 + 2;
+    b = rand() % 1000 + 2;
+    cout << "Numbers: " << a << ' ' << b << "\n";
+
+    long res1 = gcd_naive(a, b);
+    long res2 = euclid_gcd(a, b);
+    if (res1 != res2) {
+      cout << "Wrong answer: " << res1 << ' ' << res2 << "\n";
+      break;
+    }
+    else {
+      cout << "OK\n";
+    }
+  }
+  // std::cin >> a >> b;
+  // std::cout << euclid_gcd(a, b) << std::endl;
+  // return 0;
 }
