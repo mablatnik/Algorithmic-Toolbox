368776677f58b39e17f692d5edad3e3e22bb8a6c
Mark Blatnik <mablatnik@gmail.com>
2016-12-09 13:46:54 +0300

    Python solution to Changing Money Problem
    Max time used: 0.08/5.00, max memory used: 8720384/536870912

--- algrithmic_toolbox/week_3/02_greedy_algorithms_starter_files/change/change.py
+++ algrithmic_toolbox/week_3/02_greedy_algorithms_starter_files/change/change.py
@@ -1,10 +1,44 @@
 # Uses python3
-import sys
+import sys, unittest
 
-def get_change(m):
-    #write your code here
-    return m
+def get_change_specific(value):
+	p, n, d = 1, 5, 10
+	count = 0
+	while value > 0:
+		if value >= d:
+			count += value // d
+			value = value % d
+		elif value >= n:
+			count += value // n
+			value = value % n
+		else:
+			count += value // p
+			break
+	return count
+
+
+def get_change_abstract(value, *coins):
+	count = 0
+	i = 0
+	while value > 0:
+		count += value // coins[i]
+		value = value % coins[i]
+		i += 1
+	return count
+
+
+class MyTest(unittest.TestCase):
+	def test_naive(self):
+		self.assertEqual( get_change_specific(2), 2)
+		self.assertEqual( get_change_specific(28), 6)
+
+
+	def test_naive(self):
+		self.assertEqual( get_change_abstract(2, 10, 5, 1), 2)
+		self.assertEqual( get_change_abstract(28, 10, 5, 1), 6)
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+	# unittest.main()
+
+	value = int(sys.stdin.read())
+	print(get_change_specific(value))
