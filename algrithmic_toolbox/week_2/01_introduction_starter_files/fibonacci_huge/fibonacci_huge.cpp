917591d4ea4e8f7cf7657bb52c29c1a992e9a5f7
Mark Blatnik <mablatnik@gmail.com>
2016-12-09 09:32:45 +0300

    C++ solution to Compute a Huge Fibonacci Number Modulo m
    Max time used: 0.00/1.00, max memory used: 30367744/536870912

--- algrithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_huge/fibonacci_huge.cpp
+++ algrithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_huge/fibonacci_huge.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+using std::cin;
+using std::cout;
+
 long long get_fibonacci_huge_naive(long long n, long long m) {
     if (n <= 1)
         return n;
@@ -16,8 +19,31 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     return current % m;
 }
 
+long long get_pisano_period_length(long long m) {
+    long long F1 = 0, F2 = 1, F = F1 + F2;
+    for (int i = 0; i < m*m; i++) {
+        F = (F1 + F2) % m;
+        F1 = F2;
+        F2 = F;
+        if (F1 == 0 && F2 == 1) return i + 1;
+    }
+}
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    long long remainder = n % get_pisano_period_length(m);
+
+    long long F1 = 0, F2 = 1, F = remainder;
+    for(int i = 1;i<remainder;i++) {
+        F = (F1 + F2) % m;
+        F1 = F2;
+        F2 = F;
+    }
+    return F % m;
+}
+
 int main() {
     long long n, m;
+
     std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
 }
