9c85bb999e264a8e49f6838cb67f1dfa8bfd3a94
Mark Blatnik <mablatnik@gmail.com>
2016-12-09 09:57:08 +0300

    C++ solution to Compute the Least Common Multiple
    Max time used: 0.00/1.00, max memory used: 9584640/536870912

--- algrithmic_toolbox/week_2/01_introduction_starter_files/lcm/lcm.cpp
+++ algrithmic_toolbox/week_2/01_introduction_starter_files/lcm/lcm.cpp
@@ -8,9 +8,24 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int euclid_gcd(long a, long b) {
+  int divisor = a >= b ? a : b;
+  int dividend = a <= b ? a : b;
+  while(divisor != 0) {
+    int remainder = dividend % divisor;
+    dividend = divisor;
+    divisor = remainder;
+  }
+  return dividend;
+}
+
+long long lcm_fast( long long a, long long b) {
+	return (a * b)/euclid_gcd(a, b);
+}
+
 int main() {
-  int a, b;
+  long long a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
