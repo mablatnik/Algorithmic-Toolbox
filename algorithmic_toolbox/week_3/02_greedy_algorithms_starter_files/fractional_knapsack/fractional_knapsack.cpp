f7586ea8c87ed8f8fda014cb27ad9203db1d0188
Mark Blatnik <mablatnik@gmail.com>
2016-12-12 12:22:48 +0300

    C++ solution to Maximizing the Value of a Loot (Fractional Knapsack)

--- algorithmic_toolbox/week_3/02_greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.cpp
+++ algorithmic_toolbox/week_3/02_greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.cpp
@@ -3,10 +3,30 @@
 
 using std::vector;
 
+int get_max_index (vector<int> weights, vector<int> values) {
+  int max_i = 0;
+  double max = 0;
+
+  for (int i = 0; i < weights.size(); i++) {
+    if (weights[i] != 0 && (double)values[i]/weights[i] > max) {
+      max = (double)values[i]/weights[i];
+      max_i = i;
+    }
+  }
+  return max_i;
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
 
-  // write your code here
+  for (int i = 0; i < weights.size(); i++) {
+    if (capacity == 0) return value;
+    int index = get_max_index(weights, values);
+    int a = std::min(capacity, weights[index]);
+    value += a * (double)values[index]/weights[index];
+    weights[index] -= a;
+    capacity -= a;
+  }
 
   return value;
 }
