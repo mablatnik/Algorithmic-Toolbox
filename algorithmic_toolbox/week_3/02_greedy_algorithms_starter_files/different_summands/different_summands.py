edb01d0708c9a33294928c6f5d0fa070a5b62fc0
Mark Blatnik <mablatnik@gmail.com>
2016-12-13 08:25:47 +0300

    Python solution to Different Summands (Maximizing the Number of Prize Places in a Competition)
    Max time used: 0.07/5.00, max memory used: 9621504/536870912

--- algorithmic_toolbox/week_3/02_greedy_algorithms_starter_files/different_summands/different_summands.py
+++ algorithmic_toolbox/week_3/02_greedy_algorithms_starter_files/different_summands/different_summands.py
@@ -3,7 +3,16 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+    for i in range(1, n+1):
+    	n -= i
+    	if n <= i:
+    		summands.append(n+i)
+    		break
+    	elif n == 0:
+    		summands.append(i)
+    		break
+    	else:
+    		summands.append(i)
     return summands
 
 if __name__ == '__main__':
