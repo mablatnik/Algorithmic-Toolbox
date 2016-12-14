23d33aff9a7c272c09400dc10470c6ecd4ec045b
Mark Blatnik <mablatnik@gmail.com>
2016-12-14 12:26:26 +0300

    C++ iterative implementation of Binary Search
    Max time used: 0.09/2.00, max memory used: 37978112/536870912

--- algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/binary_search/binary_search.cpp
+++ algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/binary_search/binary_search.cpp
@@ -4,9 +4,16 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+int binary_search_iterative(const vector<int> &a, int x) {
+  int left = 0, right = (int)a.size();
+  while(left <= right)
+  {
+    int mid = left + (right - left)/2;
+    if(x == a[mid]) return mid;
+    else if (x < a[mid]) right = mid-1;
+    else left = mid+1;
+  }
+  return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
@@ -31,6 +38,6 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    std::cout << binary_search_iterative(a, b[i]) << ' ';
   }
 }
