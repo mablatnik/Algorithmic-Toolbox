1ee8547c0410bdc940b512cfeb9fba00dccbb9fc
Mark Blatnik <mablatnik@gmail.com>
2016-12-14 14:50:04 +0300

    Python divide and conquer solution to Majority Element
    Max time used: 0.36/5.00, max memory used: 21397504/536870912

--- algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/majority_element/majority_element.py
+++ algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/majority_element/majority_element.py
@@ -1,12 +1,30 @@
 # Uses python3
 import sys
 
+
 def get_majority_element(a, left, right):
     if left == right:
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+
+    left_elem = get_majority_element(a, left,(left + right - 1)//2+1)
+    right_elem = get_majority_element(a, (left + right - 1)//2+1, right)
+
+    lcount = 0
+    for i in range(left, right):
+        if a[i] == left_elem:
+            lcount += 1
+    if lcount > (right-left)//2:
+        return left_elem
+
+    rcount = 0
+    for i in range(left, right):
+        if a[i] == right_elem:
+            rcount += 1
+    if rcount > (right-left)//2:
+        return right_elem
+
     return -1
 
 if __name__ == '__main__':
