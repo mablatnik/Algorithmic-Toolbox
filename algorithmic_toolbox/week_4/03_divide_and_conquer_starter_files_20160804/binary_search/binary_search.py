2d38d92c0324a6899456d65248c5ce2285b56fb4
Mark Blatnik <mablatnik@gmail.com>
2016-12-14 12:05:05 +0300

    Python iterative implementation of Binary Search
    Max time used: 0.64/10.00, max memory used: 37978112/536870912

--- algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/binary_search/binary_search.py
+++ algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/binary_search/binary_search.py
@@ -1,9 +1,18 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+def binary_search_iterative(a, x):
+    left, right = 0, len(a)-1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if a[mid] == x:
+            return mid
+        elif x < a[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +28,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search_iterative(a, x), end = ' ')
