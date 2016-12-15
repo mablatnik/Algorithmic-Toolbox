2636665d06e12af6c9203487b7136096821d3068
Mark Blatnik <mablatnik@gmail.com>
2016-12-15 10:22:28 +0300

    Python solution to 3 way quick sort problem
    Max time used: 0.88/11.00, max memory used: 28889088/536870912

--- algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/sorting/sorting.py
+++ algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/sorting/sorting.py
@@ -2,35 +2,48 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
+def quick_sort_partition3(a, left, right):
+    if left < right:
+        pivot = random.randint(left, right-1)
+        pivot_value = a[pivot]
+        p_l1 = p_l2 = left
+        p_e = right
+        while p_l2 < p_e:
+            if a[p_l2] < pivot_value:
+                a[p_l2], a[p_l1] = a[p_l1], a[p_l2]
+                p_l1 += 1
+                p_l2 += 1
+            elif a[p_l2] == pivot_value:
+                p_l2 += 1
+            else:
+                p_e -= 1
+                a[p_l2], a[p_e] = a[p_e], a[p_l2]
+        quick_sort_partition3(a, left, p_l1)
+        quick_sort_partition3(a, p_e, right)
 
-def partition2(a, l, r):
-    x = a[l]
-    j = l;
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
+def partition2(a, left, right):
+    pivot = random.randint(left, right)
+    a[right], a[pivot] = a[pivot], a[right]
+    pivot_value = a[right]
+    pIndex = left;
+    for i in range(left, right):
+        if a[i] <= pivot_value:
+            a[i], a[pIndex] = a[pIndex], a[i]
+            pIndex += 1
+    a[right], a[pIndex] = a[pIndex], a[right]
+    return pIndex
 
 
-def randomized_quick_sort(a, l, r):
-    if l >= r:
-        return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+def randomized_quick_sort(a, left, right):
+    if left < right:
+        pIndex = partition3(a, left, right)
+        randomized_quick_sort(a, left, pIndex - 1);
+        randomized_quick_sort(a, pIndex + 1, right);
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
+    quick_sort_partition3(a, 0, n)
     for x in a:
         print(x, end=' ')
