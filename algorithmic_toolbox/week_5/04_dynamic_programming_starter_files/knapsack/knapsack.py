1237cf87d1acbd6deed5fc143ae09a477b8def7a
Mark Blatnik <mablatnik@gmail.com>
2016-12-20 13:17:11 +0300

    Week 5 Assignment and Python solutions to knapsack as well as primitive calculator problems
    Primitive Calculator
    Max time used: 1.08/7.50, max memory used: 13688832/536870912
    
    Knapsack (Taking as Much Gold as Possible)
    Max time used: 0.61/10.00, max memory used: 20611072/536870912

---
+++ algorithmic_toolbox/week_5/04_dynamic_programming_starter_files/knapsack/knapsack.py
@@ -0,0 +1,29 @@
+# Uses python3
+import sys
+
+def optimal_weight(W, w):
+    items = [0]
+    for item in w:
+        if item <= W:
+            items.append(item)
+
+    item_length = len(items)
+    capacity = W + 1
+
+    weights = [[0 for _ in range(item_length)] for _ in range(capacity)]
+
+    for j in range(1, item_length):
+        for i in range(1, capacity):
+            previous = weights[i][j-1]
+            current = items[j] + weights[i - items[j]][j-1]
+            if current > i:
+                weights[i][j] = previous
+            else:
+                weights[i][j] = max(previous, current)
+
+    return weights[-1][-1]
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    W, n, *w = list(map(int, input.split()))
+    print(optimal_weight(W, w))
---
+++ algorithmic_toolbox/week_5/04_dynamic_programming_starter_files/primitive_calculator/primitive_calculator.py
@@ -0,0 +1,53 @@
+# Uses python3
+import sys
+
+def optimal_sequence(n):
+    sequence = []
+    while n >= 1:
+        sequence.append(n)
+        if n % 3 == 0:
+            n = n // 3
+        elif n % 2 == 0:
+            n = n // 2
+        else:
+            n = n - 1
+    return reversed(sequence)
+
+def dynamic_sequence(n):
+    operations_count = [0] * (n + 1)
+
+    operations_count[1] = 1
+    for i in range(2, n + 1):
+        count_index = [i - 1]
+        if i % 2 == 0:
+            count_index.append(i // 2)
+        if i % 3 == 0:
+            count_index.append(i // 3)
+
+        min_count = min([operations_count[x] for x in count_index])
+        operations_count[i] = min_count + 1
+
+    current_value = n
+    value_trail = [current_value]
+    while current_value != 1:
+        option_list = [current_value - 1]
+        if current_value % 2 == 0:
+            option_list.append(current_value // 2)
+        if current_value % 3 == 0:
+            option_list.append(current_value // 3)
+
+        current_value = min(
+            [(c, operations_count[c]) for c in option_list],
+            key=lambda x: x[1]
+            )[0]
+        value_trail.append(current_value)
+    return reversed(value_trail)
+    
+    
+
+input = sys.stdin.read()
+n = int(input)
+sequence = list(dynamic_sequence(n))
+print(len(sequence) - 1)
+for x in sequence:
+    print(x, end=' ')
