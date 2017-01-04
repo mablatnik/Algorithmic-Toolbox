1a64cb7d6b79fb201c62ed0e67fb67025f19a956
Mark Blatnik <mablatnik@gmail.com>
2017-01-04 13:14:37 +0300

    Java implementation of Compute the Last Digit of a Large Fibonacci Number
    Max time used: 0.19/1.50, max memory used: 28651520/536870912

--- algorithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_last_digit/FibonacciLastDigit.java
+++ algorithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_last_digit/FibonacciLastDigit.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigitNaive(int n) {
@@ -6,7 +6,7 @@ public class FibonacciLastDigit {
             return n;
 
         int previous = 0;
-        int current  = 1;
+        int current = 1;
 
         for (int i = 0; i < n - 1; ++i) {
             int tmp_previous = previous;
@@ -16,12 +16,24 @@ public class FibonacciLastDigit {
 
         return current % 10;
     }
-    
+
+    public static int getFibonacciLastDigitFast(int n) {
+        if (n <= 1)
+            return n;
+
+        int [] result = new int[n+1];
+        result[0] = 0;
+        result[1] = 1;
+        for(int i=2;i<n+1;i++) {
+            result[i] = (result[i-1] + result[i-2]) % 10;
+        }
+        return result[n];
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        int c = getFibonacciLastDigitFast(n);
         System.out.println(c);
     }
 }
-
