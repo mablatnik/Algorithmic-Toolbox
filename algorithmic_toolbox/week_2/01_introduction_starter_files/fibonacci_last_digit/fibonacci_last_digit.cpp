0374586d26770f82f093587598bbc79b3f4797f8
Mark Blatnik <mablatnik@gmail.com>
2017-01-04 12:50:34 +0300

    C++ implementation of Compute the Last Digit of a Large Fibonacci Number
    Max time used: 0.00/1.00, max memory used: 8699904/536870912

--- algorithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_last_digit/fibonacci_last_digit.cpp
+++ algorithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_last_digit/fibonacci_last_digit.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <assert.h>
 
-int get_fibonacci_last_digit_naive(int n) {
+int get_fibonacci_last_digit_naive(long long n) {
     if (n <= 1)
         return n;
 
@@ -16,7 +17,7 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
-int get_fibonacci_last_digit_fast(int n) {
+int get_fibonacci_last_digit_fast(long long n) {
     if (n <= 1)
         return n;
 
@@ -26,22 +27,23 @@ int get_fibonacci_last_digit_fast(int n) {
     for (int i = 0; i < n -1; ++i) {
         int tmp_previous = previous % 10;
         previous = current % 10;
-        current = tmp_prevous + current % 10;
+        current = tmp_previous + current % 10;
     }
+    return current % 10;
 }
 
-void test_solution() {
-    assert(fibonacci_fast(3), 2);
-    assert(fibonacci_fast(3), 2);
-    for (int n = 0; n < 20; ++n)
-        assert(fibonacci_fast(n) == fibonacci_naive(n));
-}
+// void test_solution() {
+//     assert(get_fibonacci_last_digit_fast(3), 2);
+//     assert(get_fibonacci_last_digit_fast(3), 2);
+//     for (int n = 0; n < 20; ++n)
+//         assert(get_fibonacci_last_digit_fast(n) == get_fibonacci_last_digit_naive(n));
+// }
 
 int main() {
     int n;
     std::cin >> n;
 
-    test_solution();
-    // int c = get_fibonacci_last_digit_naive(n);
-    // std::cout << c << '\n';
-    }
+    // test_solution();
+    int c = get_fibonacci_last_digit_fast(n);
+    std::cout << c << '\n';
+}
