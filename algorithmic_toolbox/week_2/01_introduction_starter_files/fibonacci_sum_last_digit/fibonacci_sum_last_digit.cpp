6dfc6118dc0356beca86fa79e1a4f59642e8ee52
Mark Blatnik <mablatnik@gmail.com>
2017-01-06 10:38:18 +0300

    C++ implementation of Compute the Last Digit of a Sum of Fibonacci Numbers
    Max time used: 0.00/1.00, max memory used: 8720384/536870912

--- algorithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_sum_last_digit/fibonacci_sum_last_digit.cpp
+++ algorithmic_toolbox/week_2/01_introduction_starter_files/fibonacci_sum_last_digit/fibonacci_sum_last_digit.cpp
@@ -18,8 +18,33 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int get_fibonacci_last_digit_fast(long long n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i = 0; i < n -1; ++i) {
+        int tmp_previous = previous % 10;
+        previous = current % 10;
+        current = tmp_previous + current % 10;
+    }
+    return current % 10;
+}
+
+int fibonacci_sum_fast(long long n) {
+    int new_n = (n + 2) % 60;
+    int new_last = get_fibonacci_last_digit_fast(new_n);
+    if (new_last == 0) {
+        return 9;
+    } else {
+        return new_last -1;
+    }
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
