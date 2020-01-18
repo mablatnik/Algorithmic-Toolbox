b1e71b0d0d83a1f8f4e7365d0266a33defc5889a
Sudesh Chaudhary <48723832+sudesh1122@users.noreply.github.com>
2020-01-19 02:44:16 +0530

    Add fast_count_segments function
    fast_count_segments function implemented using a faster algorithm.

--- algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/points_and_segments/points_and_segments.cpp
+++ algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/points_and_segments/points_and_segments.cpp
@@ -1,38 +1,67 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+using namespace std;
+struct segment
+{
+  int start;
+  int end;
+};  
 
-using std::vector;
+vector<int> fast_count_segments(vector<pair<int,int>> &v,vector<int> points) {
+  vector<int> cnt(points.size());
+  //write your code here
+  int n1=v.size(),n2=points.size(),i=0,j=0,k;
+  for(i=0;i<n2;i++)
+  {
+    k=0;
+    for(j=0;j<n1;j++)
+    {
+      if(points[i]>=v[j].first && points[i]<=v[j].second)
+        k++;
+      else if(points[i]<v[j].first)
+        j=n1;
+    }
+    cnt[i] = k;
+  }
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-    vector<int> cnt(points.size());
-    //write your code here
-    return cnt;
+  return cnt;
 }
 
 vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-    vector<int> cnt(points.size());
-    for (size_t i = 0; i < points.size(); i++) {
-        for (size_t j = 0; j < starts.size(); j++) {
-            cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
-        }
+  vector<int> cnt(points.size());
+  for (size_t i = 0; i < points.size(); i++) {
+    for (size_t j = 0; j < starts.size(); j++) {  
+      cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
     }
-    return cnt;
+  }
+  return cnt;
 }
 
 int main() {
-    int n, m;
-    std::cin >> n >> m;
-    vector<int> starts(n), ends(n);
-    for (size_t i = 0; i < starts.size(); i++) {
-        std::cin >> starts[i] >> ends[i];
-    }
-    vector<int> points(m);
-    for (size_t i = 0; i < points.size(); i++) {
-        std::cin >> points[i];
-    }
-    //use fast_count_segments
-    vector<int> cnt = naive_count_segments(starts, ends, points);
-    for (size_t i = 0; i < cnt.size(); i++) {
-        std::cout << cnt[i] << ' ';
-    }
+  int n, m,a,b;
+  std::cin >> n >> m;
+  /*vector<int> starts(n), ends(n);
+  for (size_t i = 0; i < starts.size(); i++) {
+    std::cin >> starts[i] >> ends[i];
+  }*/
+  
+  std::vector<pair<int,int>> v;
+  for(auto i =0;i<n;i++)
+  {
+    cin>>a>>b;
+    v.push_back(make_pair(a,b));
+  }
+  
+  vector<int> points(m);
+  for (auto i = 0; i < points.size(); i++) {
+    std::cin >> points[i];
+  }
+  sort(v.begin(),v.end());
+  
+  //use fast_count_segments
+  vector<int> cnt = fast_count_segments(v, points);
+  for (size_t i = 0; i < cnt.size(); i++) {
+    std::cout << cnt[i] << ' ';
+  }
 }
